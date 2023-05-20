edd2a8e847602a2ac397598b11e170c4cb2f1119
ohhMyPrincessMomo <momo2752002@gmail.com>
2023-05-20 23:01:00 +0630

    added updateOrderStatus

--- router/OrderRoute.js
+++ router/OrderRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {authmiddleware} = require('../middlewares/authmiddleware');
-const {createOrder , getMyOrder, getMonthOrderIncome, getYearlyOrderIncome, getAllOrders, getOrderById} = require('../controllers/Order-Controller');
+const {createOrder , getMyOrder, getMonthOrderIncome, getYearlyOrderIncome, getAllOrders, getOrderById, updateOrderStatus} = require('../controllers/Order-Controller');
 router.post('/',authmiddleware,createOrder);
 router.get('/:id',authmiddleware,getOrderById)
+router.patch('/u/update_order',authmiddleware,updateOrderStatus)
 router.get('/admin/monthly-income',authmiddleware,getMonthOrderIncome)
 // router.get('/admin/monthly-count',authmiddleware,getMonthOrderCount)
 router.get('/admin/yearly-income',authmiddleware,getYearlyOrderIncome)
